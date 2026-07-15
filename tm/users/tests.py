3fd6b875a5e461febbb178186ef0a3e00676d8d8
ilsurkhal319-cell <ilsurkhal319@gmail.com>
2026-07-15 23:21:00 +0500

    Add login test

--- tm/users/tests.py
+++ tm/users/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from users.models import CustomUser
 
-# Create your tests here.
+
+class RegisterViewTests(TestCase):
+    def test_user_can_register(self):
+        response = self.client.post(reverse("users:register"), {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password1": "StrongPassword123",
+            "password2": "StrongPassword123",
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(CustomUser.objects.filter(username="testuser").exists())
+
+class LoginViewTests(TestCase):
+    def test_user_can_login(self):
+        CustomUser.objects.create_user(
+            username="testuser",
+            email="test@example.com",
+            password="StrongPassword123"
+        )
+
+        response = self.client.post(reverse("users:login"), {
+            "username": "testuser",
+            "password": "StrongPassword123",
+        })
+
+        self.assertEqual(response.status_code, 302)
