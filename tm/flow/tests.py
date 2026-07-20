6068d6e10ad0d38134b39be997dc3bfd3c503813
ilsurkhal319-cell <ilsurkhal319@gmail.com>
2026-07-20 21:16:53 +0500

    Add task creation API test

--- tm/flow/tests.py
+++ tm/flow/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from users.models import CustomUser
 from flow.models import Workspace, Board, Column, Task
 from django.urls import reverse
-
+import json
 
 class WorkspaceModelTests(TestCase):
     def test_workspace_can_be_created(self):
@@ -119,3 +119,45 @@ class BoardAccessTests(TestCase):
         response = self.client.get(reverse("flow:board_detail", args=[board.id]))
 
         self.assertNotEqual(response.context["board"], board)
+
+class TaskApiTests(TestCase):
+    def test_authenticated_user_can_create_task(self):
+        user = CustomUser.objects.create_user(
+            username="apiuser",
+            email="apiuser@example.com",
+            password="StrongPassword123"
+        )
+
+        workspace = Workspace.objects.create(
+            name="API Workspace",
+            owner=user,
+            is_personal=True
+        )
+
+        board = Board.objects.create(
+            title="API Board",
+            workspace=workspace,
+            created_by=user
+        )
+
+        column = Column.objects.create(
+            board=board,
+            title="To Do",
+            order=0
+        )
+
+        self.client.login(username="apiuser", password="StrongPassword123")
+
+        response = self.client.post(
+            reverse("flow:api_task_create"),
+            data=json.dumps({
+                "title": "Task from API",
+                "description": "Created in test",
+                "priority": "high",
+                "column_id": column.id
+            }),
+            content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(Task.objects.filter(title="Task from API").exists())
